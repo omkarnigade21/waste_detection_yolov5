edede68d521d016f24a55b52972c7de9dd1a4555
omkarnigade21 <omkarnigadedeshmukh@gmail.com>
2025-06-23 23:38:40 +0530

    added motebook experiment

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'wasteDetection',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Omkar',
+    author_email= 'omkarnigadedeshmukh@gmail.com',
     packages= find_packages(),
     install_requires = []
 
